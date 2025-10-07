7b727412e298c0f2bc553d2a8c1502610d41739a
Astalum <taaro20040526@icloud.com>
2025-10-08 05:39:48 +0900

    ホームページとユーザのダッシュボードを分離

--- app/routes.ts
+++ app/routes.ts
@@ -3,15 +3,19 @@ import { route, index } from '@react-router/dev/routes';
 
 export default [
 	// ホームページ
-	index('pages/general/home.tsx'),
+	index('pages/home.tsx'),
 	
 	// 認証関連
 	route('login', 'pages/general/login.tsx'),
 	route('create-profile', 'pages/general/create-profile.tsx'),
 	
 	// 一般機能
+	route('dashboard', 'pages/general/dashboard.tsx'),
 	route('adjustment', 'pages/general/adjustment.tsx'),
 	
 	// 管理機能
 	route('admin', 'pages/admin/admin.tsx'),
+	route('admin/users', 'pages/admin/member.tsx'),
+	route('admin/settings', 'pages/admin/settings.tsx'),
+	route('admin/manageAdjustment', 'pages/admin/manage-adjustment.tsx'),
 ] satisfies RouteConfig;
